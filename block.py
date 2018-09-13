afa191ccedc984fdceac2d67ec44b583f7bd8b3a
ultv <ultv@inbox.ru>
2018-09-13 19:14:43 +0400

    init commit

---
+++ block.py
@@ -0,0 +1,22 @@
+import json
+
+def write_block():
+    data = {'name': 'ivan',
+             'amount': 5,
+             'to_whom': 'stepan',
+             'hash': '123'}
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()

d294161c8abf8e518c11071a396f0ac072cc897a
ultv <ultv@inbox.ru>
2018-09-13 19:58:08 +0400

    write collection files

--- block.py
+++ block.py
@@ -10,7 +10,14 @@ def write_block(name, amount, to_whom, prev_hash=''):
              'to_whom': to_whom,
              'hash': prev_hash}
 
-    with open(blockchain_dir + 'test', 'w') as file:
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
+
+    last_file = files[-1]
+
+    file_name = str(last_file + 1)
+
+    with open(blockchain_dir + file_name, 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 

c77ca5e65ef31f1336e73a9434183de5370f7fc6
ultv <ultv@inbox.ru>
2018-09-15 15:29:03 +0400

    make bloks

--- flask/app/main.py
+++ flask/app/main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from block import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
         lender = request.form['lender']
         amount = request.form['amount']
         borrower = request.form['borrower']
-        
+
+        write_block(name=lender, amount=amount, to_whom=borrower)
+        return redirect(url_for('index'))
+
     return render_template('index.html')
 
 

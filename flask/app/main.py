12bdc20ea5b906532a7d363623d75a7a25cb5382
ultv <ultv@inbox.ru>
2018-09-15 15:05:39 +0400

    create templates

--- flask/app/main.py
+++ flask/app/main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    if request.method == 'POST':
+        lender = request.form['lender']
+        amount = request.form['amount']
+        borrower = request.form['borrower']
+        
     return render_template('index.html')
 
 
